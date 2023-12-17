0ca408c0419f4bf3a0b3eb37f0ccf346ff288e5c
theLiamFC <147665958+theLiamFC@users.noreply.github.com>
2023-12-17 15:50:25 -0500

    Update godsEye.py

--- godsEye.py
+++ godsEye.py
@@ -2,159 +2,75 @@ import numpy as np
 import paho.mqtt.client as mqtt
 import cv2
 import time
+from godsBrain import findTarget, findRobot, motionMonitor, imageGUI
 
-TARGET_THRESH = 50
-ROBOT_THRESH = 100
-
-def findTarget(img):
-    # Convert image to HSV color space
-    hsvFrame = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-    # HSV Color Threshold
-    # BUG needs new BLUE threshold values
-    target_lower = np.array([25, 80, 100], np.uint8)
-    target_upper = np.array([90, 255, 230], np.uint8)
-    target_mask = cv2.inRange(hsvFrame, target_lower, target_upper)
-
-    # Creating contour to track blue color
-    contours, hierarchy = cv2.findContours(
-        target_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
-    )
-
-    foundTarget = False
-    foundArea = 0
-    for contour in contours:
-        area = cv2.contourArea(contour)
-        if area > foundArea and area > TARGET_THRESH:
-            foundTarget = True
-            foundArea = area
-            x, y, w, h = cv2.boundingRect(contour)
-            scene = cv2.drawContours(scene, [contour], 0, (0, 0, 255), 5)
-
-    if foundTarget:
-        return [x, y, w, h]
-    else:
-        return [0]
+POSITION_ERROR = 5  # positioning error in pixels
+fromPico = ""
 
 
-def findRobot(img, scene):
-    # Convert image to HSV color space
-    hsvFrame = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-    # HSV Color Threshold
-    # BUG needs new GREEN threshold values
-    target_lower = np.array([25, 80, 100], np.uint8)
-    target_upper = np.array([90, 255, 230], np.uint8)
-    target_mask = cv2.inRange(hsvFrame, target_lower, target_upper)
-
-    # Creating contour to track green color
-    contours, hierarchy = cv2.findContours(
-        target_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
-    )
-
-    for contour in contours:
-        area = cv2.contourArea(contour)
-
-        if area > 100:
-            # get number of sides
-            approx = cv2.approxPolyDP(
-                contour, 0.01 * cv2.arcLength(contour, True), True
-            )
-
-            # draw contour of shapes
-            scene = cv2.drawContours(scene, [contour], 0, (0, 0, 255), 5)
-
-            # finding center point of shape
-            M = cv2.moments(contour)
-            if M["m00"] != 0.0:
-                x = int(M["m10"] / M["m00"])
-                y = int(M["m01"] / M["m00"])
-
-            if len(approx) == 4:
-                cv2.putText(
-                    img,
-                    "Base",
-                    (x, y),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.6,
-                    (255, 255, 255),
-                    2,
-                )
-                baseLoc = [x, y]
-            elif len(approx) > 6:
-                cv2.putText(
-                    img,
-                    "Magnet",
-                    (x, y),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.6,
-                    (255, 255, 255),
-                    2,
-                )
-                magnetLoc = [x, y]
-
-    return img, baseLoc, magnetLoc
-
-def motionMonitor(base,last,now):
-    w = 50 # pixel width of robot arm
-    exclusion = [base - w,base + w]
+def on_message(client, userdata, msg):
+    global fromPico
+    fromPico = msg.payload.decode()
+    print("Got message: ", fromPico)
 
 
+pico = mqtt.Client("itsGod")
+pico.connect("10.243.65.201")
+pico.on_message = on_message
+pico.subscribe("ahoy")
 
 cam = cv2.VideoCapture(0)
-time.sleep(1)
+time.sleep(3)
 
 state = "start"
+target = None
+base = None
+magnet = None
+lastFrame = None
+
+
+def pos2string(pos):
+    if len(pos) == 4:
+        return str(pos[0]) + " " + str(pos[1]) + " " + str(pos[2]) + " " + str(pos[3])
+    elif len(pos) == 3:
+        return str(pos[0]) + " " + str(pos[1]) + " " + str(pos[2])
+    elif len(pos) == 2:
+        return str(pos[0]) + " " + str(pos[1])
+
 
 while True:
     _, image = cam.read()
-    image = cv2.flip(image, 1)
+    image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
 
-    if state == "start":
-        # BUG !!! set arm position to be out of the way
-        state = "findTarget"
-    elif state == "findtarget":
-        # BUG check that arm is out of the way
-        # if it isnt reset it
+    pico.subscribe("ahoy")
 
-        targetLoc = findTarget(image)
-        if len(targetLoc) > 1:
-            state = "setX"
-    elif state == "setX":
-        if motionMonitor:
-            state = "findtarget"
-        else:
-            scene, base, magnet = findRobot(image)
+    if fromPico == "Bad message / no data":
+        print("Error: MQTT connection is bad")
 
-            # BUG adjust stepper according to base and target
+    target = findTarget(image)
+    magnet = findRobot(image)
 
-            # BUG if statement to check if within error allowance
-            state = "setY"
-    elif state == "setY":
-        if motionMonitor:
-            state = "findtarget"
-        else:
-            scene, base, magnet = findRobot(image)
+    if target and magnet[0]:
+        # motion = motionMonitor(magnet, lastFrame, image)
+        pos = [target[0], target[1], magnet[0], magnet[1]]
+        pico.publish("hello", pos2string(pos))
+        print("Published Target + Magnet: ", pos2string(pos))
+    elif target:
+        pos = [target[0], target[1]]
+        pico.publish("hello", pos2string(pos))
+        print("Published Target: ", pos2string(pos))
+    elif magnet[0]:
+        pos = ["blank", magnet[0], magnet[1]]
+        pico.publish("hello", pos2string(pos))
+        print("Published Magnet: ", pos2string(pos))
 
-            # BUG adjust stepper according to base and target
+    niceImg = imageGUI(image, target, base, magnet)
+    lastFrame = image
 
-            # BUG if statement to check if within error allowance
-            state = "retreive"
-    elif state == "retreive":
-        if motionMonitor:
-            state = "findtarget"
-        else:
-            # BUG run preprogrammed motion to lower and raise magnet
-            state = "verify"
-    elif state == "verify":
-        # BUG get the arm out of the way
-        
-        # BUG search area of target for blue color
-        # if there is blue color below threshold target has been retrieved
-        # do something to end program
+    time.sleep(0.01)
 
     # Program Termination
-    cv2.imshow("GodsEye", image)
+    cv2.imshow("GodsEye", niceImg)
     if cv2.waitKey(10) & 0xFF == ord("q"):
         cam.release()
         cv2.destroyAllWindows()
